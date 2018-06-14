542cbe7c402c5bbc3875b554704f162cc561c8f5
Gvl Naga Saikiran <saikiranpresident@gmail.com>
2018-06-14 19:31:10 +0530

    Day 3 Changes

--- html/scripts/plugin-activate.js
+++ html/scripts/plugin-activate.js
@@ -17,4 +17,14 @@ $(document).ready(function() {
   });
   //mobile menu
   $(".mobile_menu").simpleMobileMenu();
+  // smoothscroll
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+     if(target.length) {
+         event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
+  });
 });
