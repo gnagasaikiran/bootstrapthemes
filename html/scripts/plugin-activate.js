1c9a2ab5bbb0ee3982b001e6b759e45952ebfe2b
Gvl Naga Saikiran <saikiranpresident@gmail.com>
2018-07-02 12:55:45 +0530

    Final Bootstrap Theme

--- html/scripts/plugin-activate.js
+++ html/scripts/plugin-activate.js
@@ -10,17 +10,19 @@ $(document).ready(function() {
   });
   // bxslider sscript 
   $('.bxslider').bxSlider({
-  	  // auto: true,
+  	  //auto: true,
       mode: 'horizontal',
-      infiniteLoop: true,
-      controls: false,
+      // infiniteLoop: true,
+      controls: true,
+      pager: false,
       speed: 1000
   });
   //portfolio script
   $('.portfolio-images').slick({
+      //infinite: false,
       slidesToShow: 3,
       slidesToScroll: 1,
-      autoplay: true,
+      autoplay: true,     
       autoplaySpeed: 1000,
        responsive: [
       {
@@ -34,7 +36,7 @@ $(document).ready(function() {
   //mobile menu
   $(".mobile_menu").simpleMobileMenu();
   //form validation
-  //$("#formsimplevalidation").simpleValidation();
+  $("#formsimplevalidation").simpleValidation();
   // smoothscroll
   $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
@@ -45,4 +47,40 @@ $(document).ready(function() {
           }, 1000);
       }
   });
+  // Back top Script start //
+   $("#back-to-top").hide();
+   // fade in #back-to-top
+   $(window).scroll(function() {
+     if ($(this).scrollTop() > 100) {
+       $("#back-to-top").fadeIn();
+     } else {
+       $("#back-to-top").fadeOut();
+     }
+   });
+// scroll body to 0px on click
+   $("#back-to-top").click(function() {
+     $("body,html").animate({
+       scrollTop: 0
+     }, 800);
+     return false;
+   });
 });
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("myBtn").style.display = "block";
+//     } else {
+//         document.getElementById("myBtn").style.display = "none";
+//     }
+// }
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+
+
+
+
